369cdf6473846b868f504c20db43ddab827f5a6e
Carlos Trindade <carlos.trindade@estudante.ifms.edu.br>
2021-03-16 21:05:32 -0400

    Inicializando o template da aplicação

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>Hello world</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import url('https://fonts.googleapis.com/css2?family=Asap:wght@400;500;600;700&display=swap');
+    * {
+      font-family: 'Asap', sans-serif;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+
+      height: 100vh;
+      display: grid;
+    }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
+
 import App from './App.vue'
+
+import './config/bootstrap'
+
+import router from './config/router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
